4ff87ee205d31444b4aeff3c7fb8cf04c34b1764
h-emma <emmah92@hotmail.com>
2022-04-27 14:37:32 +0200

    Update App.js
    Add components, and function to remove duplicates and add data from my api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Card from "./components/Card/Card.js";
+import { H1 } from "./components/Text/Text";
 
 function App() {
   const [result, setResult] = useState();
+  const [podfiles, setPodfiles] = useState();
 
   useEffect(() => {
     const url = "https://api.sr.se/api/v2/podfiles?programid=5067&format=json";
@@ -12,20 +15,35 @@ function App() {
       .then((data) => {
         console.log(data);
         setResult(data);
+
+        const fliterPodfile = [];
+
+        for (let i = 0; i < data.podfiles.length; i++) {
+          if (i === 0) {
+            fliterPodfile.push(data.podfiles[0]);
+            continue;
+          }
+          if (data.podfiles[i].title === data.podfiles[i - 1].title) {
+            continue;
+          }
+          fliterPodfile.push(data.podfiles[i]);
+        }
+        setPodfiles(fliterPodfile);
       });
   }, []);
 
   return (
     <div className="App">
-      <h1>Här hittar du de senaste avsnitten av p3 Historia</h1>
+      <H1>Här hittar du de senaste avsnitten av p3 Historia</H1>
       {result &&
-        result.podfiles.map((podfile, i) => {
+        podfiles.map((podfile, i) => {
           return (
-            <div key={i}>
-              <p>{podfile.title}</p>
-              <p>{podfile.description}</p>
-              <a href={podfile.url}>Ladda ner avsnittet</a>
-            </div>
+            <Card
+              key={i}
+              url={podfile.url}
+              title={podfile.title}
+              description={podfile.description}
+            />
           );
         })}
     </div>
