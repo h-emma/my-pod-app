d491d4135de70b5912e57d1478edce956b692b58
h-emma <emmah92@hotmail.com>
2022-04-27 14:38:53 +0200

    Create styles.js

---
+++ src/components/Image/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ImageStyled = styled.img`
+  width: 253px;
+  height: 253px;
+  box-shadow: 8px 8px 4px rgba(0, 0, 0, 0.25);
+  margin-top: 38px;
+`;
