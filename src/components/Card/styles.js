7d67befa61f594e827cd41b369f802230d4c68c7
h-emma <emmah92@hotmail.com>
2022-04-27 14:38:11 +0200

    Create styles.js

---
+++ src/components/Card/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const CardContainerStyled = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 599px;
+  height: 636px;
+  border-radius: 8px;
+  background-color: #ffede0;
+`;
+
+export const CardDescriptionStyled = styled.div`
+  width: 427px;
+  text-align: center;
+`;
+
+export const CardTitleStyled = styled.div`
+  width: 349px;
+  height: 123px;
+  text-align: center;
+`;
