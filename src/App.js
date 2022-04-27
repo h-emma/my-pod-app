8897ed71d5d2cc2723a6d3c23fac0cbf4479fe97
h-emma <emmah92@hotmail.com>
2022-04-27 08:51:32 +0200

    add content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [result, setResult] = useState();
+
+  useEffect(() => {
+    const url = "http://api.sr.se/api/v2/podfiles?programid=5067&format=json";
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setResult(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Här hittar du de senaste avsnitten av p3 Historia</h1>
+      {result &&
+        result.podfiles.map((podfile, i) => {
+          return (
+            (<p key={i}>{podfile.title}</p>),
+            (<p key={i}>{podfile.description}</p>),
+            (<p key={i}>{podfile.url}</p>)
+          );
+        })}
+      <a href="http://sverigesradio.se/topsy/ljudfil/srapi/7365817.mp3">
+        Ladda ner avsnittet
+      </a>
     </div>
   );
 }
