1ccd887bc04529ebd80a063e636b3fc1c379d1d0
h-emma <emmah92@hotmail.com>
2022-04-27 14:38:03 +0200

    Create styles.js

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ButtonStyled = styled.button`
+  width: 188px;
+  height: 55px;
+  background-color: #ffb19a;
+  border-radius: 24px;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
+`;
+
+export const LinkStyled = styled.a`
+  margin: 16px 0px 0px 0px;
+`;
