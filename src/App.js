88be28ab5ec117d3cf312aca2ea73594c85ad147
h-emma <emmah92@hotmail.com>
2022-04-28 13:18:25 +0200

    Add div around main title

--- src/App.js
+++ src/App.js
@@ -34,7 +34,9 @@ function App() {
 
   return (
     <div className="App">
-      <H1>Här hittar du de senaste avsnitten av p3 Historia</H1>
+      <div className="Header">
+        <H1>Här hittar du avsnitt av p3 Historia</H1>
+      </div>
       {result &&
         podfiles.map((podfile, i) => {
           return (
